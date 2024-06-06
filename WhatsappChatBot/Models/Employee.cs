3ccc4a29be2efcc443e8cc0747ded20d941923e6
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-06-07 01:12:35 +0530

    Models and db context

---
+++ WhatsappChatBot/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WhatsappChatBot.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? DepartmentName { get; set; }
+}
---
+++ WhatsappChatBot/Models/WhatsAppDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WhatsappChatBot.Models;
+
+public partial class WhatsAppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public WhatsAppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public WhatsAppDbContext(DbContextOptions<WhatsAppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_configuration.GetConnectionString("WhatsAppDB"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3213E83F6093E586");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.DepartmentName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("department_name");
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
